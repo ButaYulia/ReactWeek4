52213a63805b0745bb43ade0a829a690062c6ba4
Yuliia <buta.yulia@gmail.com>
2023-06-26 21:27:14 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [city, setCity] = useState("");
+  const [forecast, setForecast] = useState("");
+
+  function displayWeather(response) {
+    setForecast(
+      <div>
+        <ol>
+          <li>🌡️ Temperature: {Math.round(response.data.main.temp)}°C</li>
+          <li>📙 Description: {response.data.weather[0].description}</li>
+          <li>💧 Humidity: {response.data.main.humidity}%</li>
+          <li>🌬️ Wind: {Math.round(response.data.wind.speed)}km/h</li>
+          <li>
+            <img
+              src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              alt={response.data.weather[0].description}
+            />
+          </li>
+        </ol>
+      </div>
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=894a2e7aa7f46eeca5d8778f6faa5a5b&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Search Engine</h1>
+      <div className="CitySearch">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input type="submit" value="search" />
+        </form>
+        <h2>{forecast}</h2>
+      </div>
     </div>
   );
 }
-
-export default App;
